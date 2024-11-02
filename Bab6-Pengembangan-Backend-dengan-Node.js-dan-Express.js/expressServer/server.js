ba70dfd76605d617a50ec0d3cd327b224ebeed40
amlly38 <amaliyahamel0308@gmail.com>
2024-11-02 16:02:29 +0700

    feat(expressServer): learn middleware with express.js)

--- Bab6-Pengembangan-Backend-dengan-Node.js-dan-Express.js/expressServer/server.js
+++ Bab6-Pengembangan-Backend-dengan-Node.js-dan-Express.js/expressServer/server.js
@@ -1,13 +1,30 @@
 import express from 'express'
+//import bodyParser from 'body-parser'
 const app = express()
 const port = 3000
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const headerMiddleware = function (req, res, next) {
+  try {
+    console.log(req.headers);
+    next();
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/contact', (req, res) => {
+app.get('/contact', headerMiddleware, (req, res) => {
   res.send('<h1>Contact Me</h1>')
+})
+
+app.post('/submit', (req, res) => {
+  console.log(req.body)
 })
 
 app.listen(port, () => {
