428756177679526512a97ee95ddb4d5276eaf5eb
amlly38 <amaliyahamel0308@gmail.com>
2024-11-02 15:01:28 +0700

    feat(bab6/expressServer): learning http request

--- Bab6-Pengembangan-Backend-dengan-Node.js-dan-Express.js/expressServer/server.js
+++ Bab6-Pengembangan-Backend-dengan-Node.js-dan-Express.js/expressServer/server.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Me</h1>')
 })
 
 app.listen(port, () => {
